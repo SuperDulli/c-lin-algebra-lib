9013a11eb87a550c4d4e7556ec62701928b5fe60
chelmerd <chelmerd@student.42wolfsburg.de>
2022-07-21 16:48:19 +0200

    fix vector subtraction

--- vector/sub.c
+++ vector/sub.c
@@ -6,7 +6,7 @@
 /*   By: chelmerd <chelmerd@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 16:37:32 by chelmerd          #+#    #+#             */
-/*   Updated: 2022/07/13 16:39:24 by chelmerd         ###   ########.fr       */
+/*   Updated: 2022/07/21 16:47:15 by chelmerd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ float	*vec3_sub(float *v1, float *v2, float *result)
 {
 	result[0] = v1[0] - v2[0];
 	result[1] = v1[1] - v2[1];
-	result[1] = v1[2] - v2[2];
+	result[2] = v1[2] - v2[2];
 	return (result);
 }
 
@@ -29,7 +29,7 @@ float	*vec4_sub(float *v1, float *v2, float *result)
 {
 	result[0] = v1[0] - v2[0];
 	result[1] = v1[1] - v2[1];
-	result[1] = v1[2] - v2[2];
-	result[1] = v1[3] - v2[3];
+	result[2] = v1[2] - v2[2];
+	result[3] = v1[3] - v2[3];
 	return (result);
 }
