2951a5c2870223498c8f8af705291f7a92193af0
chelmerd <chelmerd@student.42wolfsburg.de>
2022-08-29 20:26:13 +0200

    replace custum min and max functions with standard

--- vector/clamp.c
+++ vector/clamp.c
@@ -6,32 +6,16 @@
 /*   By: chelmerd <chelmerd@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 11:52:13 by chelmerd          #+#    #+#             */
-/*   Updated: 2022/08/10 11:59:02 by chelmerd         ###   ########.fr       */
+/*   Updated: 2022/08/29 20:22:39 by chelmerd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static
-float	ft_maxf(float a, float b)
-{
-	if (a > b)
-		return (a);
-	else
-		return (b);
-}
-
-static
-float	ft_minf(float a, float b)
-{
-	if (a < b)
-		return (a);
-	else
-		return (b);
-}
+#include <math.h>
 
 float	*vec3_clamp(float *vec, float lower, float upper, float *result)
 {
-	result[0] = ft_maxf(lower, ft_minf(vec[0], upper));
-	result[1] = ft_maxf(lower, ft_minf(vec[1], upper));
-	result[2] = ft_maxf(lower, ft_minf(vec[2], upper));
+	result[0] = fmaxf(lower, fminf(vec[0], upper));
+	result[1] = fmaxf(lower, fminf(vec[1], upper));
+	result[2] = fmaxf(lower, fminf(vec[2], upper));
 	return (result);
 }
